0a75491a3a862abb944ce8af88cf4ae56ca92cf6
Rostyslav Vozniak <rostik19wozniak@icloud.com>
2024-06-26 09:58:02 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+/** @format */
+
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+import tailwind from '@astrojs/tailwind';
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
+      apiOptions: {}, // storyblok-js-client options
+      components: {
+        page: 'storyblok/Page',
+        hero: 'storyblok/Hero',
+        text_image_section: 'storyblok/TextImageSection',
+        config: 'storyblok/Config',
+        teaser: 'storyblok/Teaser',
+        'popular-articles': 'storyblok/PopularArticles',
+        'all-articles': 'storyblok/AllArticles',
+        article: 'storyblok/Article',
+      },
+      componentsDir: 'src',
+      enableFallbackComponent: false,
+      customFallbackComponent: '',
+      useCustomApi: false,
+      apiOptions: {
+        region: 'eu',
+      },
+    }),
+  ],
+  vite: {
+    server: {
+      https: true,
+    },
+
+    plugins: [mkcert()],
+  },
+  output: 'server',
+});
